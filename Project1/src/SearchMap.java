bcff9efd82839a1df233a70b8b8158ce1aa446d2
Jonathan Yiu <jonathanyiu@MyMacs-MBP.lan1>
2018-09-17 02:09:09 -0700

    get cost from src to dest

--- Project1/src/SearchMap.java
+++ Project1/src/SearchMap.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class SearchMap {
-    public class Graph
-    {
+    public class Graph {
         HashMap<String, ArrayList<String> > adjListArray;
         HashMap<String, Integer> edgeWeights;
             
@@ -23,14 +22,12 @@ public class SearchMap {
             // }
         }
 
-        public void addVertex(String name)
-        {
+        public void addVertex(String name) {
             adjListArray.put(name, new ArrayList<String>());
         }
 
         // Adds an edge to a directed graph
-        public void addEdge(String src, String dest, int cost)
-        {
+        public void addEdge(String src, String dest, int cost) {
             if(!adjListArray.containsKey(src)) {
                 this.addVertex(src);
             }
@@ -41,6 +38,14 @@ public class SearchMap {
             this.adjListArray.get(src).add(dest);
             this.edgeWeights.put(src + " " + dest, cost);
         }
+
+        public ArrayList<String> adjacentTo(String v) {
+            if(adjListArray.containsKey(v)) {
+                return adjListArray.get(v);
+            }
+            return null;
+        }
+
         // A utility function to print the adjacency list 
         // representation of graph
         public void printGraph()
@@ -74,6 +79,59 @@ public class SearchMap {
             } 
         }
     }
+
+    public class PathFinder {
+
+        // prev[v] = previous vertex on shortest path from s to v
+        // dist[v] = length of shortest path from s to v
+        private HashMap<String, String>  prev = new HashMap<String, String>();
+        private HashMap<String, Integer> dist = new HashMap<String, Integer>();
+
+        // run BFS in graph G from given source vertex s
+        public PathFinder(Graph G, String s) {
+
+            // put source on the queue
+            Queue<String> queue = new LinkedList<String>();
+            queue.add(s);
+            dist.put(s, 0);
+            
+            // repeated remove next vertex v from queue and insert
+            // all its neighbors, provided they haven't yet been visited
+            while (!queue.isEmpty()) {
+                String v = queue.poll();
+                for (String w : G.adjacentTo(v)) {
+                    if (!dist.containsKey(w)) {
+                        queue.add(w);
+                        dist.put(w, 1 + dist.get(v));
+                        prev.put(w, v);
+                    }
+                }
+            }
+        }
+
+        // is v reachable from the source s?
+        public boolean hasPathTo(String v) {
+            return dist.containsKey(v);
+        }
+
+        // return the length of the shortest path from v to s
+        public int distanceTo(String v) {
+            if (!hasPathTo(v)) return Integer.MAX_VALUE;
+            return dist.get(v);
+        }
+
+        // return the shortest path from v to s as an Iterable
+        public ArrayList<String> pathTo(String v) {
+            ArrayList<String> path = new ArrayList<String>();
+            while (v != null && dist.containsKey(v)) {
+                path.add(v);
+                v = prev.get(v);
+            }
+            return path;
+        }
+    }
+
+
     public static void main(String[] args) throws IOException{
         try
         {
@@ -106,5 +164,21 @@ public class SearchMap {
 
         graph.printGraph();
         graph.printCosts();
+
+        System.out.println("Testing Pathfinder...");
+
+        PathFinder pf = new PathFinder(graph, start);
+        
+        ArrayList<String> pth = pf.pathTo("Z");
+
+        int cost = 0;
+        String last = start;
+        System.out.print(pth.get(pth.size()-1));
+        for (int i=pth.size()-2; i>=0; i--) {
+            System.out.print("   " + pth.get(i));
+            cost += graph.edgeWeights.get(last + " " + pth.get(i));
+            last = pth.get(i);
+        }
+        System.out.println("\nCost: " + cost + "\n");
     }
 }
